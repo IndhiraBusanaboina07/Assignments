fa66f9c9808d7b85a7c93f45c578086d70bb4165
IndhiraBusanaboina07 <123065939+IndhiraBusanaboina07@users.noreply.github.com>
2023-09-27 21:30:41 +0530

    Update Assignment 6.py

--- Assignment 6.py
+++ Assignment 6.py
@@ -19,6 +19,34 @@ class LinkedList:
     def isValidPos(self, pos):
         
         return (pos<=self.size)
+    def insert(self, val, pos):
+        
+        
+        if(self.size == 0 and pos == 1):
+            new_node = Node(val)
+            self.head = new_node
+        
+        print(self.isValidPos(pos))
+        elif(isValidPos(pos) == False):
+            print(self.isValidPos(pos))
+            print("checked")
+            return "Invalid Position"
+        
+        else: 
+            
+            
+            ref = self.head
+            count = 1
+            
+            while(count != pos):
+                ref = ref.next
+                count += 1
+            
+            new_node = Node(val)
+            new_node.next = ref.next
+            ref.next = new_node
+        
+        self.size += 1    
                    
 def test():
  linked_list_1 = LinkedList()
