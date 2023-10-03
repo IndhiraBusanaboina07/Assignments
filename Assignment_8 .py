79cffd1fec8b2998d130e260c4142e99712d9883
IndhiraBusanaboina07 <123065939+IndhiraBusanaboina07@users.noreply.github.com>
2023-10-03 20:28:22 +0530

    Add files via upload

--- Assignment_8 .py
+++ Assignment_8 .py
@@ -1,32 +1,90 @@
+
 class Node:
-    def __init__(self, value,next=None, previous=None):
-        self._value=value
-        self._next=next
-        self._previous=previous
-        
+    def _init_(self,data):
+        self.data=data
+        self.nextnode=None
 class LinkedList:
-    def __init__(self):
-        self._first=None
+    def _init_(self):
+        self.head=None
+        self.size=0
+        self.tail = None
+        self.nodebeforetail = None
 
-    def append(self, value):
-        if self._first==None: 
-            self._first=Node(value)
+    def insert_at_start(self,data):
+        newnode=Node(data)
+        self.size+=1
+        if not self.head:
+            self.head=newnode
         else:
-            n=self._first
-            while n._next:
-                n=n._next
-            n._next=Node(value, previous=n)
+            newnode.nextnode=self.head
+            self.head=newnode
 
-    def info(self):
-        if self._first==None: 
+    def remove_last(self):
+        if self.head is None:
+            return None 
+        if self.head == self.tail:
+            removed_data = self.head.data
+            self.head = None
+            self.tail = None
+            return removed_data
+        current = self.head
+        while current.nextnode != self.tail:
+            current = current.nextnode
+        removed_data = self.tail.data
+        current.nextnode = None
+        self.tail = current
+        return removed_data
+            
+            
+          
+    def size(self):
+        return self.size
+
+    def append(self,data):
+        temp=self.head
+        newnode=Node(data)
+        if self.head is None:
+            self.head=newnode
+            self.tail = newnode
+            self.size+=1
+            print(f"Appended {self.tail.data}")
+        else:
+           
+            self.tail.nextnode = newnode
+            self.tail=newnode
+            self.size+=1
+            print(f"Appended {self.tail.data}")
+
+    def count(self,value):
+        temp=self.head
+        count=0
+        while(temp!=None ):
+            if temp.data==value:
+                count+=1
+            temp=temp.nextnode
+        return count
+    
+    def print_info(self):
+        if self.head==None: 
             return "LinkedList(empty)"
         str="LinkedList(\t"
-        n=self._first
+        n=self.head
         while n:
-            str+=f'{n._value}\t'
-            n=n._next
+            str+=f'{n.data}\t'
+            n=n.nextnode
         str+=")"
-        return str
+        print(str)
+        
+ll = LinkedList()
+ll.append(10)
+ll.append(10)
+ll.append(11)
+ll.append(12)
 
-  
-  
+ll.print_info()
+
+size=ll.sizee()
+print(f'The size of linked list is {size}')
+
+ll.print_info()
+print(f"size {ll.sizee()}")
