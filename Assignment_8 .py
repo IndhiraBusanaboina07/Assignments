536674e403950e7aa36be65dee65534970591012
IndhiraBusanaboina07 <123065939+IndhiraBusanaboina07@users.noreply.github.com>
2023-10-03 10:38:00 +0530

    Update Assignment_8 .py

--- Assignment_8 .py
+++ Assignment_8 .py
@@ -36,21 +36,5 @@ class LinkedList:
             n=n._next
         return c
     
-    def node_at_pos(list, pos):
-        n=list._first
-        for i in range(pos):
-            n=n._next
-            if n==None:
-                break
-        else:
-            return n
-    
-    def get(list,index):
-        node = list.node_at_pos(index)
-        return node._value
-
-    def set(list,index,value):
-        node = list.node_at_pos(index)
-        node._value=value
-        
+  
   
