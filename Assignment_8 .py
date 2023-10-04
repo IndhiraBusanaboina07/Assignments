9487544ae89a3868fa7fe90cca9270cc4d50b5ee
IndhiraBusanaboina07 <123065939+IndhiraBusanaboina07@users.noreply.github.com>
2023-10-04 09:55:02 +0530

    Update Assignment_8 .py

--- Assignment_8 .py
+++ Assignment_8 .py
@@ -19,61 +19,7 @@ class LinkedList:
             newnode.nextnode=self.head
             self.head=newnode
 
-    def remove_last(self):
-        if self.head is None:
-            return None 
-        if self.head == self.tail:
-            removed_data = self.head.data
-            self.head = None
-            self.tail = None
-            return removed_data
-        current = self.head
-        while current.nextnode != self.tail:
-            current = current.nextnode
-        removed_data = self.tail.data
-        current.nextnode = None
-        self.tail = current
-        return removed_data
-            
-            
-          
-    def size(self):
-        return self.size
-
-    def append(self,data):
-        temp=self.head
-        newnode=Node(data)
-        if self.head is None:
-            self.head=newnode
-            self.tail = newnode
-            self.size+=1
-            print(f"Appended {self.tail.data}")
-        else:
-           
-            self.tail.nextnode = newnode
-            self.tail=newnode
-            self.size+=1
-            print(f"Appended {self.tail.data}")
-
-    def count(self,value):
-        temp=self.head
-        count=0
-        while(temp!=None ):
-            if temp.data==value:
-                count+=1
-            temp=temp.nextnode
-        return count
     
-    def print_info(self):
-        if self.head==None: 
-            return "LinkedList(empty)"
-        str="LinkedList(\t"
-        n=self.head
-        while n:
-            str+=f'{n.data}\t'
-            n=n.nextnode
-        str+=")"
-        print(str)
         
 ll = LinkedList()
 ll.append(10)
