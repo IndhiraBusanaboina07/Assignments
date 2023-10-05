02ab238348ea179a3b2b097e39c96470fbfd1f73
IndhiraBusanaboina07 <123065939+IndhiraBusanaboina07@users.noreply.github.com>
2023-10-05 09:31:31 +0530

    Update Assignment 9_3.py

--- Assignment 9/Assignment 9_3.py
+++ Assignment 9/Assignment 9_3.py
@@ -1,78 +1,251 @@
 class Book:
-  
-   def __init__(self,id,title,author,price,rating):
-        self.id=id
-        self.title=title
-        self.author=author
-        self.price=price 
-        self.rating=rating
 
-class Book_store:
+    def __init__(self, id, title, author, price, rating, next=None):
 
-  def __init__(self):
-        self.books=[]
-  
-  def find_book_by_id(self, book_id):
-        for book in self._books:
-            if(book._id == book_id):
-                 print(f"{book.id},{book.title},{book.author},{book.price},{book.rating}")
-            
-        return None
+        self.id =id
 
-  def find_book_by_author(self, author_name):
-       
-        
-        for book in self._books:
-            if(book._author == author_name):
-               print(f"{book.id},{book.title},{book.auBoothor},{book.price},{book.rating}")
-        return None
+        self.title = title
 
-  def find_book_by_rating(self, less_rating, high_rating):
-        
-        
-        for book in self._books:
-            if(less_rating <= book._rating and high_rating >= book._rating):
-                 print(f"{book.id},{book.title},{book.author},{book.price},{book.rating}")
-                
-        return None
+        self.author= author
 
+        self.price = price
 
-  def find_book_by_price(self, low_price, high_price):
-      
-        
-       for book in self._books:
-           if(low_price <= book._price and book._price <= high_price):
-                print(f"{book.id},{book.title},{book.author},{book.price},{book.rating}")
-                
-       return None
-  
-  def info(self):
-        print( f''Book Id: {self._id}, \tBook Name: {self._title},\tAuthor: {self._author},\tPrice: {self._price},\tRating: {self._rating}\n'')
+        self.rating = int(rating)
 
-  
-  def add_book(self,book):
-       self.books.append(book)
+        self.next = next
 
-book1 = Book(1, 'python','Tom', 150,4.3)
-book2 = Book(2, 'SQL','Bob', 100, 2.5)
-book3 = Book(3, 'Datascience','Mick',300,4.5)
-
-s=Book_store()
-s.add_book(book1)
-s.add_book(book2)
-s.add_book(book3)
-
-s.info()
-
-s.find_book_by_author("Bob")
-s.find_book_by_rating(3,5)
-
-s.find_book_by_price(50,200)
-
-s. find_book_by_id(102)
-    
-                
-                
-       
-    
  
+
+class LinkedList_book:
+
+    def __init__(self):
+
+        self.first = None
+
+ 
+
+    def addBooks(self, book):
+
+        if not self.first:
+
+            self.first = book
+
+        else:
+
+            current_node = self.first
+
+            while current_node.next:
+
+                current_node = current_node.next
+
+            current_node.next = book
+
+ 
+
+    def findBookByID(self, id):
+
+        current_node = self.first
+
+        while current_node:
+
+            if current_node.id == id:
+
+                return current_node
+
+            current_node = current_node.next
+
+        return False
+
+   
+
+    def findBooksByAuthor(self, author):
+
+        list_books = list()
+
+        current_node = self.first
+
+        while current_node:
+
+            if current_node.author == author:
+
+                list_books.append(current_node)
+
+            current_node = current_node.next
+
+        return list_books
+
+   
+
+    def findBooksByRating(self, min_rating, max_rating):
+
+        list_books = list()
+
+        current_node = self.first
+
+        while current_node:
+
+            if current_node.rating>=min_rating and current_node.rating<= max_rating:
+
+                list_books.append(current_node)
+
+            current_node = current_node.next
+
+        return list_books
+
+   
+
+    def findBooksByPrice(self, min_price, max_price):
+
+        list_book = list()
+
+        current_node = self.first
+
+        while current_node:
+
+            if current_node.price >= min_price and current_node.price <= max_price:
+
+                list_book.append(current_node)
+
+            current_node=current_node.next
+
+        return list_book
+
+ 
+
+   
+
+    def display_books(self):
+
+        current_book = self.first
+
+        while current_book:
+
+            print(f"Book ID: {current_book.id}, Title of the Book: {current_book.title}, Author: {current_book.author}, Price: {current_book.price}, Rating: {current_book.rating}")
+
+            current_book = current_book.next
+
+   
+
+ 
+
+if __name__ == "__main__":
+
+    BookData = LinkedList_book()
+
+    Book1  = Book(1,"Ramayan","Valmiki",5000, 5)
+
+    Book2  = Book(2,"Mahabharath","Vedvyas",6000, 5)
+
+    Book3  = Book(3, "Wings of Fire", "APJ Abdul Kalam", 365 ,4.5)
+
+    Book4  = Book(4, "Ignited Minds", "APJ Abdul Kalam", 263 ,3)
+
+ 
+
+    BookData.addBooks(Book1)
+
+    BookData.addBooks(Book2)
+
+    BookData.addBooks(Book3)
+
+    BookData.addBooks(Book4)
+
+    id = 3
+
+    found_book = BookData.findBookByID(id)
+
+    if found_book:
+
+        print(f"\nBook with ID:{found_book.id} is {found_book.title}", end="")
+
+    else:
+
+        print(f"Book with {id} is Not found")
+
+    print("\n")
+
+    print("*"*100)
+
+ 
+
+    author = "APJ Abdul Kalam"
+
+    found_book_a = BookData.findBooksByAuthor(author)
+
+    if found_book_a:
+
+        print(f"Books by {author} are", end=":\t")
+
+        for buk in found_book_a:        
+
+            print(f"{buk.title}", end="\t")
+
+    else:
+
+        print("No books found")
+
+   
+
+    print("\n")
+
+    print("*"*100)
+
+ 
+
+    min_rating = 4
+
+    max_rating = 5
+
+    found_book=BookData.findBooksByRating(int(min_rating), int(max_rating))
+
+    if found_book:
+
+        print(f"Books with rating between {min_rating} and {max_rating} are: ", end="\t")
+
+        for buk in found_book:
+
+            print(f"{buk.title}", end = "\t")
+
+    else:
+
+        print("books not found")
+
+ 
+
+    print("\n")
+
+    print("*"*100)
+
+ 
+
+    min_price = 200
+
+    max_price = 1000
+
+    found_book=BookData.findBooksByPrice(min_price, max_price)
+
+    if found_book:
+
+        print(f"Books with prices between {min_price} and {max_price} are: ", end="\t")
+
+        for buk in found_book:
+
+            print(f"{buk.title}", end = "\t")
+
+    else:
+
+        print("books not found")
+
+ 
+
+    print("\n")
+
+    print("*"*100)
+
+   
+
+   
+
+    BookData.display_books()
+
+   
